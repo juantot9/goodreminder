239bf9ac82c0061790a6941d7e9c2658fe8a76a0
juantot9 <juaaratri@gmail.com>
2021-05-02 16:56:50 +0200

    prueba

--- lib/src/login_page.dart
+++ lib/src/login_page.dart
@@ -12,126 +12,124 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Column(
-            children: [Padding(
+          body: Center(
+        child: Column(
+          children: [
+            Padding(
               padding: const EdgeInsets.all(20.0),
-              child: Image.asset('images/logo.png',
-              height: 100.0,
+              child: Image.asset(
+                'images/logo.png',
+                height: 100.0,
               ),
             ),
-            SizedBox(height: 15.0,),
-             _userTextField(),
-            SizedBox(height: 15.0,),
-             _passwordTextField(),
-            SizedBox(height: 20.0,),
-             _buttonLogin(),
-            SizedBox(height: 20.0,),
-             _buttonRegister(),
-            SizedBox(height: 20.0,),
-             _buttonUnregister(),
-            ],
-          ),
-        )
-      ),
+            SizedBox(
+              height: 15.0,
+            ),
+            _userTextField(),
+            SizedBox(
+              height: 15.0,
+            ),
+            _passwordTextField(),
+            SizedBox(
+              height: 20.0,
+            ),
+            _buttonLogin(),
+            SizedBox(
+              height: 20.0,
+            ),
+            _buttonRegister(),
+            SizedBox(
+              height: 20.0,
+            ),
+            _buttonUnregister(),
+          ],
+        ),
+      )),
     );
   }
 
   Widget _userTextField() {
     return StreamBuilder(
-      builder:(BuildContext context, AsyncSnapshot snapshot){
-        return Container(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          child: TextField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              icon: Icon(Icons.email),
-              hintText: 'ejemplo@correo.com',
-              labelText: 'Correo electrogeno',
-            ), 
-            onChanged: (value){
-
-            },
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: TextField(
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            icon: Icon(Icons.email),
+            hintText: 'ejemplo@correo.com',
+            labelText: 'Correo electrogeno',
           ),
-        );
-      } 
-    );
+          onChanged: (value) {},
+        ),
+      );
+    });
   }
 
   Widget _passwordTextField() {
     return StreamBuilder(
-      builder:(BuildContext context, AsyncSnapshot snapshot){
-        return Container(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          child: TextField(
-            keyboardType: TextInputType.emailAddress,
-            obscureText: true,
-            decoration: InputDecoration(
-              icon: Icon(Icons.lock),
-              hintText: 'Contraseña',
-              labelText: 'Contraseña',
-            ), 
-            onChanged: (value){
-
-            },
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: TextField(
+          keyboardType: TextInputType.emailAddress,
+          obscureText: true,
+          decoration: InputDecoration(
+            icon: Icon(Icons.lock),
+            hintText: 'Contraseña',
+            labelText: 'Contraseña',
           ),
-        );
-      } 
-    );
+          onChanged: (value) {},
+        ),
+      );
+    });
   }
-  
 
   Widget _buttonLogin() {
     return StreamBuilder(
-      builder:(BuildContext context, AsyncSnapshot snapshot){
-        return ElevatedButton(
-           style: ElevatedButton.styleFrom(
-            minimumSize: const Size(300, 50),
-          ),
-          child: Container(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(300, 50),
+        ),
+        child: Container(
           //  padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
-            child: Text('Iniciar Sesion'),
-          ),
-          onPressed: (){},
-        );
-      } 
-    );
+          child: Text('Iniciar Sesion'),
+        ),
+        onPressed: () {},
+      );
+    });
   }
-   
-   Widget _buttonRegister() {
+
+  Widget _buttonRegister() {
     return StreamBuilder(
-      builder:(BuildContext context, AsyncSnapshot snapshot){
-        return ElevatedButton(
-           style: ElevatedButton.styleFrom(
-            minimumSize: const Size(300, 50),
-          ),
-          child: Container(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(300, 50),
+        ),
+        child: Container(
           //  padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
-            child: Text('Registrarse'),
-          ),
-          onPressed: (){},
-        );
-      } 
-    );
+          child: Text('Registrarse'),
+        ),
+        onPressed: () {},
+      );
+    });
   }
 
- Widget _buttonUnregister() {
+  Widget _buttonUnregister() {
     return StreamBuilder(
-      builder:(BuildContext context, AsyncSnapshot snapshot){
-        return ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            minimumSize: const Size(300, 50),
-          ),
-          child: Container(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(300, 50),
+        ),
+        child: Container(
           //  padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
-            child: Text('Entrar Sin Registro'),
-          ),
-
-          onPressed: (){},
-        );
-      } 
-    );
+          child: Text('Entrar sin registro'),
+        ),
+        onPressed: () {},
+      );
+    });
   }
-
-
 }
