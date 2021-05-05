a1923e2d79a6c56f7c5b63e598ab62d1d1d2bc6d
juantot9 <juaaratri@gmail.com>
2021-05-05 20:52:30 +0200

    cambio de version de flutter y lineas faltantes en gradle de android

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _fbApp = FirebaseApp.initializeApp();
+  final FirebaseApp _fbApp = Firebase.initializeApp();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
