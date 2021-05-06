c7038483baee5d2016e5f3f22f94f7defe479faf
Casto Rodríguez <casrodia@gmail.com>
2021-05-06 13:59:36 +0200

    pequeños cambios

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:good_reminder/screens/authenticate/authenticate.dart';
-import 'package:good_reminder/screens/home/home.dart';
 
 class Wrapper extends StatelessWidget {
   @override
