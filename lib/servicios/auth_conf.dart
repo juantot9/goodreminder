f343cfdc6e62cfcbef9528b3fdbd7d8ca47cdeb6
Casto Rodríguez <casrodia@gmail.com>
2021-05-06 14:45:25 +0200

    models y usario

--- lib/servicios/auth_conf.dart
+++ lib/servicios/auth_conf.dart
@@ -1,13 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:good_reminder/models/usuario.dart';
 
 class AuthConfigurationService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  Usuario _userFromFirebaseUser(User user){
+    return user!=null ? Usuario(uid: user.uid) : null;
+  }
 
   Future signInAnon() async {
     try {
       UserCredential res = await _auth.signInAnonymously();
       User user = res.user;
-      return user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;
